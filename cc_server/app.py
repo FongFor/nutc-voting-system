80f4c4e314f0d50b7b99652797d8d384c9661f00
hsieh <hsiehfengti@gmail.com>
2026-03-10 15:08:13 +0800

    test

---
+++ cc_server/app.py
@@ -0,0 +1,21 @@
+from flask import Flask
+
+
+
+app = Flask(__name__)
+
+
+
+@app.route('/')
+
+def home():
+
+    return "測試"
+
+
+
+if __name__ == '__main__':
+
+    # host='0.0.0.0' 非常重要！這樣 Docker 容器外的電腦才連得進去
+
+    app.run(host='0.0.0.0', port=5000)
